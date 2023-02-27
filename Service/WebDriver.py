ba3a7d4b73e790e453007f88e7f074efe9ecaa17
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-02-27 23:27:48 +0800

    合併 linux windows 程式

--- Service/WebDriver.py
+++ Service/WebDriver.py
@@ -4,28 +4,43 @@ from bs4 import BeautifulSoup
 from os import system
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
+from sys import platform
+
+system_commands = {
+    'win32': ["start chrome --remote-debugging-port=9222",
+              ChromeDriverManager().install(),
+              "start chrome https://nhentai.net",
+              "taskkill /F /IM chrome* /T"],
+    'linux': ["chromium-browser --disable-infobars --disable-gpu --remote-debugging-port=9222 'https://nhentai.net' &",
+              "/usr/lib/chromium-browser/chromedriver",
+              "chromium-browser 'https://nhentai.net'",
+              "killall chromium-browse"]
+}[platform]
 
 class WebDriver:
     driver = None
 
     @classmethod
     def create_driver(cls):
-        system("start chrome --remote-debugging-port=9222")
+        
+        system(system_commands[0])
+
         options = Options()
         options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-        cls.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options, keep_alive=True)
+
+        cls.driver = webdriver.Chrome(system_commands[1], options=options, keep_alive=True)
 
     @staticmethod
     def _solve_challenge():
-        system("start chrome https://nhentai.net")
+        system(system_commands[2])
         sleep(6)
-    
+
     @classmethod
     def get_soup(cls, url) -> BeautifulSoup:
         try:
             cls.driver.get(url)
         except:
-            system("taskkill /F /IM chrome* /T")
+            system(system_commands[3])
             cls.create_driver()
             cls.driver.get(url)
         soup = BeautifulSoup(cls.driver.page_source, "html.parser")
@@ -35,4 +50,3 @@ class WebDriver:
             cls._solve_challenge()
             soup = cls.get_soup()
         return soup
-
